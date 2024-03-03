52f9638e203ba2518059b6356d5c6694ae37477f
countchrisdo <crburwell@yahoo.com>
2024-03-03 14:37:02 -0500

    2nd scene change

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -15,9 +15,11 @@ func _ready():
 	$AnimatedSprite2D.play("front_idle")
 
 func _physics_process(delta):
+	#calling functions here instead bc this is every frame
 	player_movement(delta)
 	enemy_attack()
 	attack()
+	current_camera()
 	
 	if health <= 0:
 		player_alive = false
@@ -139,3 +141,12 @@ func _on_deal_attack_timer_timeout():
 	$deal_attack_timer.stop()
 	global.player_current_attack = false
 	attack_ip = false
+	
+func current_camera():
+	if global.current_scene == "world":
+		$world_camera.enabled = true
+		$cliffside_camera.enabled = false
+	elif global.current_scene == "cliff_side":
+		$world_camera.enabled = false
+		$cliffside_camera.enabled = true
+	
